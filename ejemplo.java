eef527ecbafde5aba4f29838d956e7d89f2ff56b
sebaxxs19 <1031654149@teinco.edu.co>
2025-03-26 10:48:38 -0500

    Add files via upload

---
+++ ejemplo.java
@@ -0,0 +1,13 @@
+public class Ejemplo {
+
+    public static int sumar(int a,int b){
+        return a+b:
+    }
+
+    public static void main(String[] args) {
+        int resultados = sumar (15.333):
+        System.out.println(resultados)
+    
+    }
+}
+    
